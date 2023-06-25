f540f572ecdb90cd4a20b95c9ec4f455e2c98ecb
Combatd <combatdstar@gmail.com>
2023-06-25 09:19:18 -0700

    Remove .js extension on UserModule import to AppComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { UserModule } from './user/user.module.js';
+import { UserModule } from './user/user.module';
 
 @NgModule({
   declarations: [
